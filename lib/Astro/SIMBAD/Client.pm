0d1cc74f56bbaa90368dcb4dd34c04684db77daf
Tom Wyant <wyant@cpan.org>
2012-01-05 16:59:21 +0000

    Import of WYANT/Astro-SIMBAD-Client-0.025 from CPAN.
    gitpan-cpan-distribution: Astro-SIMBAD-Client
    gitpan-cpan-version:      0.025
    gitpan-cpan-path:         WYANT/Astro-SIMBAD-Client-0.025.tar.gz
    gitpan-cpan-author:       WYANT
    gitpan-cpan-maturity:     released

--- lib/Astro/SIMBAD/Client.pm
+++ lib/Astro/SIMBAD/Client.pm
@@ -85,7 +85,7 @@ BEGIN {
     }
 }
 
-our $VERSION = '0.024';
+our $VERSION = '0.025';
 
 our @CARP_NOT = qw{Astro::SIMBAD::Client::WSQueryInterfaceService};
 
@@ -1358,7 +1358,7 @@ Thomas R. Wyant, III (F<wyant at cpan dot org>)
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2005-2011 by Thomas R. Wyant, III
+Copyright (C) 2005-2012 by Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
